eb7c6a23256025d904f960bf7442af245c716a98
Mikita Bobryk <mikita.bobryk@gmail.com>
2019-03-27 12:25:16 +0300

    Initial commit

---
+++ StaggingwebApplication/StaggingwebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StaggingwebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
